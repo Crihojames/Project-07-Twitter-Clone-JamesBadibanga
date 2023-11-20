41287387fe740910a66787172a17972b0214bb70
Crihojames <101941972+Crihojames@users.noreply.github.com>
2023-11-20 10:24:39 +0000

    my fistr commît

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
 
-import('./style/reset.css')
-import('./style/App.css')
+import('./style/reset.css');
+import('./style/App.css');
+
 export default function App() {
   return (
     <Layout>
-        <Home/>
+      <Home />
     </Layout>
-  )
+  );
 }
+//king
