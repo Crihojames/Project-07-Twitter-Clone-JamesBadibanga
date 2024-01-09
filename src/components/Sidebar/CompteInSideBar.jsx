39ccd8214e93013049b5723f142f426df76b4135
Crihojames <crihojames@gmail.com>
2024-01-10 00:52:27 +0100

    quelque modification sur la section sidebar finition

--- src/components/Sidebar/CompteInSideBar.jsx
+++ src/components/Sidebar/CompteInSideBar.jsx
@@ -1,17 +1,19 @@
 import Avatar from "../TimeLine/Avatar"
-import More2 from "../../images/More-2.svg"
+import GroupCADENA from "../../images/GroupCADENA.svg"
+// import More-2 from "../../images/More-2.svg"
 
 export default function CompteInSideBar() {
   return (
     <div className='compte-in-sidebar'>
         <div className="compte-in-sidebar-avatar"><Avatar /></div>
-        <div>
+        <div className="title-icon">
             <div className="compte-icon">
                 <p>Bradly Ortiz</p>
-                <img src={More2} alt="" />
+                <img src={GroupCADENA} alt="" />
             </div>
             <span className="tweet-title-details">@bradly</span>
         </div>
+        <div className="compMortIcon"><img src="src\images\More-2.svg" alt="" /></div>
     </div>
   )
 }
