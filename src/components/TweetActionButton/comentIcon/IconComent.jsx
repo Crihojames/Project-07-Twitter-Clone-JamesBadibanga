16d778a21b1f829343c0297b12e40209d307ad94
Crihojames <crihojames@gmail.com>
2024-01-11 09:41:51 +0100

    quelque modification de tweetActionButton sur le composent oconComment

--- src/components/TweetActionButton/comentIcon/IconComent.jsx
+++ src/components/TweetActionButton/comentIcon/IconComent.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import { useState } from 'react';
-import comment1 from "../../../images/reply.svg";
-import comment2 from "../../../images/reply.png";
+import commentIcon from "../../../images/reply.svg";
+import commentedIcon from "../../../images/reply.png";
 
 
-export default function IconComent({className,counts}) {
-  const [isLiked, setIsLiked] = useState(false)
+export default function IconComment({ counts }) {
+  const [isCommented, setIsCommented] = useState(false)
   const [count, setCount] = useState(counts)
 
   const handleClick = () => {
-    setIsLiked(!isLiked)
-    setCount(isLiked ? count - 1 : count + 1)
+    setIsCommented(!isCommented)
+    setCount(isCommented ? count - 1 : count + 1)
   }
 
   const divStyles = {
-    color: isLiked ? "#1D9BF0" : " ",
+    color: isCommented ? "#1D9BF0" : " ",
     padding: "10px",
   }
 
   return (
     <div
       className="tweet-action-button-react"
-      title="like"
+      title="comment"
       onClick={handleClick}
     >
       <img
-        src={isLiked ? comment2 : comment1}
+        src={isCommented ? commentedIcon : commentIcon}
       />
       <span style={divStyles}>{count}</span>
     </div>
