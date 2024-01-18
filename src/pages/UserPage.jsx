ef930901670ff34728e3a8ecaf800c44e6905564
Crihojames <crihojames@gmail.com>
2024-01-18 11:33:35 +0100

    useparam pour les utilisateur

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom'
 import data from '../Data/data'
 import Sidebar from '../components/Sidebar/Sidebar'
 import TrendsProfil from '../components/Profil/TrendsProfil'
+import BackButton from '../components/Buttonback'
 
 
 
@@ -13,33 +14,46 @@ export default function UserPage() {
   const post = data.find((post) => post.id == id)
   return (
     <>
-        <Sidebar />
-        <   div className='timeline' key={post.id}>
-                <div className="header">
-                    <h1>User</h1>
-                </div>
-                <div className="image-background-profil">
-                    <div className="tweet-avatar">
-                        <img src={post.avatarTweet} alt="avatar" />
-                    </div>
-                </div>
-                <div className='tweet' >
-                    <div className="tweet-content">
-                        <div className="tweet-body">
-                            <div className="tweet-title">
-                                <span className='tweet-title-author'>{post.titleTweet}</span>
-                                <span><img src={post.certificat} alt="certificated" /></span>
-                                <span className='tweet-title-details'>{post.detailsTitleTweet}</span>
-                                <span className='tweet-title-details'>{post.time}</span>
-                            </div>
-                            <div className="tweet-text">
-                                {post.textTweet}
+    <Sidebar />
+        <div className='timeline' key={post.id}>
+            <div className="header">
+                <span className='back-header'>
+                    <span className="back-button">
+                        <Link to={'/'}><BackButton /></Link>
+                    </span>
+                    <h1>
+                      {post.auteur}
+                    </h1>
+                </span>
+            </div>
+
+
+            <div className="image-background-profil image-background-profil-user">
+                <img src={post.avatarTweet} alt="avatar" />
+            </div>
+
+            <div className="tweet">
+                <div className="profil-hedear-title">
+                    <div className="userprofil">
+                        <div className="tweet-content">
+                            <div className="tweet-body">
+                                <div className="tweet-title">
+                                    <span className='tweet-title-author'>{post.titleTweet}</span>
+                                    {post.certificat ? (<span><img src="/images/Vector.svg" alt="certificated" /></span>):(<span>-</span>)}
+                                    <span className='tweet-title-details'>{post.detailsTitleTweet}</span>
+                                    <span className='tweet-title-details'>{post.time}</span>
+                                </div>
+                                <div className="tweet-text">
+                                    {post.textTweet}
+                                </div>
+                                {post.imageTweet && <span className='tweet-image'><img src={post.imageTweet} alt=""/></span>}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        <TrendsProfil />
-    </>
+        </div>
+    <TrendsProfil />
+</>
   )
 }
